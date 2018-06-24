72a65bac0176139d8abc4959a53b90b4b45351cd
unknown <38482481+cstimler@users.noreply.github.com>
2018-06-24 09:24:42 -0700

    Second commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # TeaTime Code
 
-This is a exercise repository for the TeaTime example app which is part of Udacity's Advanced Android course. TeaTime is a mock tea ordering app that demonstrates various uses of the Espresso Testing framework (i.e. Views, AdapterViews, Intents, IdlingResources). You can learn more about how to use this repository [here](https://classroom.udacity.com/courses/ud857/lessons/8b2a9d63-0ff5-48ff-90d3-a9855b701dae/concepts/41b82e3c-2797-46e5-8a66-684098ca8cbb).
+This is a exercise repository for the TeaTime example app which is part of Udacity's Advanced Android course. TeaTime is a mock tea ordering app that demonstrates various uses of the Espresso Testing framework (i.e. Views, AdapterViews, Intents, IdlingResources). 
+
+This second commit provides espresso testing code for an adapter view (specifically GridView).  The espresso test mirrors a user clicking on a tea item in the GridView in the MenuActivity and then checks to see that the appropriate corresponding tea name is the header on the OrderActivity screen that follows.  In this code, a specific position is chosen to check this correspondence (here position zero should correspond to "Black Tea").  This code also has a user defined helper matcher function that is placed here for reference but is not actually needed/used in the implementation.
--- app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
+++ app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
@@ -16,6 +16,31 @@
 
 package com.example.android.teatime;
 
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.example.android.teatime.model.Tea;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.core.deps.guava.base.Predicates.instanceOf;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+
+
 /**
  * This test demos a user clicking on a GridView item in MenuActivity which opens up the
  * corresponding OrderActivity.
@@ -25,16 +50,42 @@ package com.example.android.teatime;
  * takes into account Idling Resources.
  */
 
-
 // TODO (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
+@RunWith(AndroidJUnit4.class)
 public class MenuActivityScreenTest {
+// adapted from https://medium.com/google-developers/adapterviews-and-espresso-f4172aa853cf
+    public static Matcher withName(final Matcher nameMatcher) {
+        return new TypeSafeMatcher<Tea>() {
+            @Override
+            public boolean matchesSafely(Tea tea) {
+                return nameMatcher.matches(tea.getTeaName());
+            }
+
+            @Override
+            public void describeTo(Description description) {
+            }
+        };
+    }
 
     // TODO (2) Add the rule that provides functional testing of a single activity
+    @Rule
+    public ActivityTestRule<MenuActivity> menuActivityMenuTestRule= new ActivityTestRule<>(MenuActivity.class);
 
     // TODO (3) Finish writing this test which will click on a gridView Tea item and verify that
     // the OrderActivity opens up with the correct tea name displayed.
-
+    @Test
     public void clickGridViewItem_OpensOrderActivity() {
+       // onView(withId(R.id.tea_grid_view)).perform(click());
+      //  onData(allOf(is(instanceOf(String.class),("Black Tea")))).perform(click());
+     //   onData(withId(R.id.tea_grid_view)).inAdapterView(withName(is("Black Tea"))).perform(click());
+        // onData(is(instanceOf(MenuActivity.class))).inAdapterView(withId(R.id.grid_item_layout)).atPosition(0).perform(click());
+       // onData(is(instanceOf(String.class))).inAdapterView(withId(R.id.grid_item_layout)).atPosition(0).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.tea_grid_view)).atPosition(0).perform(click());
+
+
+
+        onView(withId(R.id.tea_name_text_view)).check(matches(withText("Black Tea")));
+
 
     }
 
